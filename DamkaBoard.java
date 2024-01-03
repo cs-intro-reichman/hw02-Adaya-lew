ac845a8af4f2ef517c11abdf279b6ce868c701d6
Adaya-lew <adaya.lewartowski@post.runi.ac.il>
2024-01-03 08:03:51 +0200

    fix damka
    fix 3

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -9,11 +9,11 @@ public class DamkaBoard {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if ( i % 2 == 0 ){
-                    System.out.print("* ");
-                }
-                else if ((i == n-1) && (j == n-1) && (i % 2 == 0)){
+                if ((i == n-1) && (j == n-1) && (i % 2 == 0)){
                     System.out.print("*");
+                }
+                else if (i % 2 == 0) {
+                    System.out.print("* ");
                 }
                 else {
                     System.out.print(" *");
